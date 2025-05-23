b4b89c17c32e764c80ad5ce1db2d7275fef55b89
Marcus Santos <marcus.xtreme03@gmail.com>
2025-05-22 21:29:58 -0300

    :sparkles: Criação função post

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
+
 
 class HelloApiView(APIView):
   """Test Api View"""
+  serializer_class = serializers.HelloSerializer
   def get(self, request, format= None):
     """Return a list of ApiView Features"""
     an_apiview = [
@@ -12,3 +16,13 @@ class HelloApiView(APIView):
       'Is mapped manually to URLs',
     ]
     return Response({'mensage': 'Hello!', 'an_apiview': an_apiview}) #Precisa estar em uma lista ou dicionario para converter para json
+  def post(self,request):
+    """Create a hello mensage with our name"""
+    serializer = self.serializer_class(data=request.data)
+    if serializer.is_valid():
+      name = serializer.validated_data.get('name')
+      message = f'Hello {name}'
+      return Response({'message': message})
+    else:
+      return Response(serializer.errors,
+             status=status.HTTP_400_BAD_REQUEST)
