03e0905acc7ecca9abae1a74518987924b72cb61
Marcus Santos <marcus.xtreme03@gmail.com>
2025-05-26 22:19:30 -0300

    :wrench: chore: Add UserProfileViewSet on urls

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import DefaultRouter
 from .views import HelloViewSet, HelloApiView
 
 router = DefaultRouter()
-router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
-
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset') # Base name só é usado quando a viewset não possui queryset
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
   path('hello-apiview/', views.HelloApiView.as_view()),
